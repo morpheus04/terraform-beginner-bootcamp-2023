adbc0f6ed7fc6902852b692ef1a7febecd3e0e05
morpheus04 <37842433+morpheus04@users.noreply.github.com>
2023-10-02 19:49:03 +0100

    41 terratowns provider (#44)
    * #41 wip setting up the skeleton for creating a custom terraform provider
    
    * #41 Implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "morpheus-io"
@@ -15,7 +21,13 @@ terraform {
   } */
 }
 
-module "terrahouse_aws" {
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+/* module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
@@ -23,4 +35,4 @@ module "terrahouse_aws" {
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   content_version = var.content_version
-}
+} */
