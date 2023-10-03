4ed24318dee9cb213a01ac54a8faa02318b20588
morpheus04 <37842433+morpheus04@users.noreply.github.com>
2023-10-03 18:37:04 +0100

    #43 Test our terra home on terratowns test town (#48)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -7,7 +7,7 @@ variable "user_uuid" {
   }
 }
 
-variable "bucket_name" {
+/* variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 
@@ -18,7 +18,7 @@ variable "bucket_name" {
     )
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
   }
-}
+} */
 
 variable "index_html_filepath" {
   description = "The file path for index.html"
