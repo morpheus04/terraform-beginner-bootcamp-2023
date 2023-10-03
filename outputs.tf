5c7c9db78ffc75f719a715be6581af9aa5a7ebf2
morpheus04 <morpheus.lion04@gmail.com>
2023-10-03 17:35:43 +0000

    #43 Test our terra home on terratowns test town

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-/* output "bucket_name" {
+output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
@@ -11,4 +11,4 @@ output "s3_website_endpoint" {
 output "cloudfront_url" {
   description = "The CloudFront Distribution Domain Name"
   value = module.terrahouse_aws.cloudfront_url
-} */
+}
