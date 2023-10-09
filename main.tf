dc1b6fad85f63915aeb912a2da08e4665c0adc4d
morpheus04 <morpheus.lion04@gmail.com>
2023-10-09 12:10:16 +0000

    #45 Built test page into new home category

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     terratowns = {
-      source = "local.providers/local/terratowns"
+      source  = "local.providers/local/terratowns"
       version = "1.0.0"
     }
   }
@@ -13,7 +13,7 @@ terraform {
   #    name = "terraform-house"
   #  }
   #}
-   cloud {
+  cloud {
     organization = "morpheus-io"
     workspaces {
       name = "terraform-house"
@@ -23,46 +23,46 @@ terraform {
 
 
 provider "terratowns" {
-  endpoint = var.terratowns_endpoint
+  endpoint  = var.terratowns_endpoint
   user_uuid = var.teacherseat_user_uuid
-  token = var.terratowns_access_token
+  token     = var.terratowns_access_token
 }
 
 module "home_arcanum_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.arcanum.public_path
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.arcanum.public_path
   content_version = var.arcanum.content_version
 }
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
-  description = <<DESCRIPTION
+  name            = "How to play Arcanum in 2023!"
+  description     = <<DESCRIPTION
 Arcanum is a game from 2001 that shipped with alot of bugs.
 Modders have removed all the originals making this game really fun
 to play (despite that old look graphics). This is my guide that will
 show you how to play arcanum without spoiling the plot.
 DESCRIPTION
-  domain_name = module.home_arcanum_hosting.domain_name
-  town = "missingo"
+  domain_name     = module.home_arcanum_hosting.domain_name
+  town            = var.arcanum.town
   content_version = var.arcanum.content_version
 }
 
-module "home_payday_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.payday.public_path
+/* module "home_payday_hosting" {
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.payday.public_path
   content_version = var.payday.content_version
 }
 
 resource "terratowns_home" "home_payday" {
-  name = "Making your Payday Bar"
-  description = <<DESCRIPTION
+  name            = "Making your Payday Bar"
+  description     = <<DESCRIPTION
 Since I really like Payday candy bars but they cost so much to import
 into Canada, I decided I would see how I could my own Paydays bars,
 and if they are most cost effective.
 DESCRIPTION
-  domain_name = module.home_payday_hosting.domain_name
-  town = "missingo"
+  domain_name     = module.home_payday_hosting.domain_name
+  town            = var.payday.town
   content_version = var.payday.content_version
-}
+}  */
